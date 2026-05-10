56e3030bd15aec81ba15f79bc6531a0c6bce8f31
soharab102 <hossainsoharab102@gmail.com>
2026-05-11 01:56:15 +0600

    luckyNumber

---
+++ codeforce/luckyNumber.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+bool isLucky(int n)
+{
+    while(n > 0)
+    {
+        int digit = n % 10;
+
+        if(digit != 4 && digit != 7)
+        {
+            return false;
+        }
+
+        n = n / 10;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+
+    bool found = false;
+
+    for(int i = A; i <= B; i++)
+    {
+        if(isLucky(i))
+        {
+            cout << i << " ";
+            found = true;
+        }
+    }
+
+    if(!found)
+    {
+        cout << -1;
+    }
+
+    return 0;
+}
