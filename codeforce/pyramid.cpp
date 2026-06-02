87eedb46011f8d15169073dcfc3015cc68659ce7
soharab102 <hossainsoharab102@gmail.com>
2026-06-02 13:26:14 +0600

    solved shape-2 problem

--- codeforce/pyramid.cpp
+++ codeforce/pyramid.cpp
@@ -5,7 +5,13 @@ int main() {
     cin>>N;
 
     for(int i=0;i<N;i++){
+        for(int j=0;j<N-i-1;j++){
+            cout<<" ";
+        }
         for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<i;j++){
             cout<<"*";
         }
         cout<<endl;
